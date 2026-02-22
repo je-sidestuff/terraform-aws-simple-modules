339890ea88cd5c2be398e0e3090e658f945d5e0c
je-sidestuff <gerald.halimak@gmail.com>
2026-02-22 14:52:20 +0000

    fix: Support http/s the ALB.

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -7,12 +7,26 @@ resource "aws_security_group" "this" {
   description = "Security group for ALB"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "HTTP from anywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.enable_http ? [1] : []
+    content {
+      description = "HTTP from anywhere"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = var.enable_https ? [1] : []
+    content {
+      description = "HTTPS from anywhere"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
